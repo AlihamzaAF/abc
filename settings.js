1f6ba493bc30fa31204bbed9a4156c930f445c18
Ali hamza <alihamza324473@gmail.com>
2025-05-27 12:11:43 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,5 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=32pQlDLS#EM2MbhJvScMPazpSfBf3MAlkrFxUzwuv-5rKwhdhFqI' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
-POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=AQ53VLga#o9GjEf1yvpk20yClAM7bofslgL3Wg9XaJd_NNnnvC_0' : process.env.POSTGRESQL_URL,
 };
